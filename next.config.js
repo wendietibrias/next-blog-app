80dff2bda5d2c2864dbfa6ecf61e34ea5a4e5b90
wendietibrias <wendietibrias@gmail.com>
2023-07-09 12:03:57 +0700

    deploy confing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+     output:"export",
+     distDir:".next",
      images: {
       domains: ['media.graphassets.com'],
+      unoptimized:true
     },
 }
 
